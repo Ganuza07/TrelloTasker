73fc4b0d71ff4eb90742e3fa27d85c95df29cf65
Ganuza_ <agustinganuza@gmail.com>
2020-08-22 19:26:49 -0300

    Setup of a Trello webhook. Added a controller.

---
+++ src/TrelloTasker.Api/Controllers/TrolleWebhookController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TrelloTasker.Model.DTO;
+
+namespace TrelloTasker.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrolleWebhookController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> OnWebhookCallBack(TrelloWebhookDTO trelloWebhookDTO)
+        {
+            if (trelloWebhookDTO.action.data.card.name.EndsWith("Bi-daily"));
+            {
+                throw new NotImplementedException();
+            }
+            return new OkObjectResult(trelloWebhookDTO);
+            throw new NotImplementedException();
+        }
+    }
+}
